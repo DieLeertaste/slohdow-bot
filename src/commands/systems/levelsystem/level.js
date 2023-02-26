196eb16c9caa053ccab2671a6a082748317b41e5
Noah <noah.p.kleineweischede@gmail.com>
2023-02-26 17:50:44 +0100

    Transfer to other PC

---
+++ src/commands/systems/levelsystem/level.js
@@ -0,0 +1,3 @@
+const db = require('../../../database.js');
+
+db.execute('INSERT INTO user VALUE (1, "lol", "lel")');
--- src/test.js
+++
@@ -1,4 +0,0 @@
-const db = require("src/database.js");
-
-db.execute('INSERT INTO user VALUE (1, "lol", "lel")')
-
